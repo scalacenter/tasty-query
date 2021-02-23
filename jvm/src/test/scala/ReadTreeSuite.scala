824bccafd372d497c7e78ce8cf14e697761a26b2
Ekaterina Goltsova <cache-nez@yandex.ru>
2021-02-23 18:20:45 +0500

    Check the unpickled tree structure in the unpickler test

--- jvm/src/test/scala/ReadTreeSuite.scala
+++ jvm/src/test/scala/ReadTreeSuite.scala
@@ -1,11 +1,13 @@
-import tastyquery.ast.Trees.Tree
+import tastyquery.ast.Names.{SimpleName, QualifiedName, TypeName}
+import tastyquery.ast.Trees._
+import tastyquery.ast.Types.{TermRef, TypeRef}
 import tastyquery.reader.TastyUnpickler
 
 import java.nio.file.{Files, Paths}
 
 class ReadTreeSuite extends munit.FunSuite {
   val ResourceProperty = "test-resources"
-  
+
   def unpickle(tastyBytes: Array[Byte]): List[Tree] = {
     val unpickler = new TastyUnpickler(tastyBytes)
     unpickler.unpickle(new TastyUnpickler.TreeSectionUnpickler()).get.unpickle
@@ -14,13 +16,35 @@ class ReadTreeSuite extends munit.FunSuite {
   def getResourcePath(name: String): String =
     s"${System.getProperty(ResourceProperty)}/$name.tasty"
 
-  // TODO: add structure checks on the unpickling result
-  def checkUnpickle(name: String): Unit =
+  def checkUnpickle(name: String, p: PartialFunction[Tree, Unit]): Unit =
     test(name) {
       val resourcePath = getResourcePath(name)
       val bytes = Files.readAllBytes(Paths.get(resourcePath))
-      unpickle(bytes)
+      val tree = unpickle(bytes).head
+      assert(p.isDefinedAt(tree))
     }
 
-  checkUnpickle("EmptyClass")
+  val isJavaLangObject: PartialFunction[Tree, Unit] = {
+    case Apply(Select(
+    New(TypeTree(TypeRef(
+    TermRef(_, QualifiedName(_, SimpleName("java"), SimpleName("lang"))),
+    TypeName(SimpleName("Object"))))),
+    _), List()) =>
+  }
+
+  checkUnpickle("EmptyClass", {
+    case PackageDef(_, List(
+    TypeDef(TypeName(SimpleName("EmptyClass")),
+    Template(
+    // default constructor: no type params, no arguments, empty body
+    DefDef(SimpleName("<init>"), List(), List(), TypeTree(_), EmptyTree),
+    // a single parent -- java.lang.Object
+    List(parent),
+    // self not specified => EmptyValDef
+    EmptyValDef,
+    // empty body
+    List()
+    )))
+    ) if isJavaLangObject.isDefinedAt(parent) =>
+  })
 }
