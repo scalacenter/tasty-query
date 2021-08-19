b16d919fdbafdb24131f440e4c7bf8caf595d224
Ekaterina Goltsova <cache-nez@yandex.ru>
2021-08-19 16:22:51 +0200

    Remove the placeholder DummySymbol

--- shared/src/main/scala/tastyquery/ast/Symbols.scala
+++ shared/src/main/scala/tastyquery/ast/Symbols.scala
@@ -4,8 +4,6 @@ import tastyquery.ast.Names.{Name, TypeName}
 
 object Symbols {
   val NoSymbol = new Symbol(Names.EmptyTermName)
-  // A placeholder for symbols that are not correctly created by tasty-query
-  val DummySymbol = new Symbol(Names.EmptyTermName)
 
   class Symbol(val name: Name) {
     override def toString: String = s"symbol[$name]"
