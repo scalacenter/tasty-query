9bf9afc3c3371d960bd8f6804498c84c98fb9005
Ekaterina Goltsova <cache-nez@yandex.ru>
2021-09-07 13:03:45 +0200

    Add two reading modes to jvm/Main: standalone .tasty file and whole project
    --standalone takes a single .tasty file and directly runs TreeUnpickler on it.
    -cp takes a classpath and runs ProjectReader on that project.
    By default, jvm/Main runs project reader on this project.

--- jvm/src/main/scala/Main.scala
+++ jvm/src/main/scala/Main.scala
@@ -1,17 +1,32 @@
 import java.nio.file.{Files, Paths}
-
 import tastyquery.reader.TastyUnpickler
 import tastyquery.Contexts
+import tastyquery.api.ProjectReader
+
+import java.net.URL
+import java.net.URLClassLoader
 
 object Main {
   def main(args: Array[String]): Unit =
-    if (args.isEmpty) {
-      println("Please enter a tasty file to read")
-    } else {
-      val filename = args(0)
-      val unpickler = new TastyUnpickler(Files.readAllBytes(Paths.get(filename)))
-      println(
-        unpickler.unpickle(new TastyUnpickler.TreeSectionUnpickler()).get.unpickle(using Contexts.empty(filename))
-      )
+    args.toList match {
+      case Nil =>
+        // By default, read this project
+        val cl = ClassLoader.getSystemClassLoader
+        val classpath = cl.asInstanceOf[URLClassLoader].getURLs.map(_.getFile).toList
+        val reader = new ProjectReader
+        reader.read(classpath)
+      case "-cp" :: classpath =>
+        val reader = new ProjectReader
+        reader.read(classpath)
+      case "--standalone" :: filename :: Nil =>
+        val unpickler = new TastyUnpickler(Files.readAllBytes(Paths.get(filename)))
+        println(
+          unpickler.unpickle(new TastyUnpickler.TreeSectionUnpickler()).get.unpickle(using Contexts.empty(filename))
+        )
+      case _ =>
+        println("""Two modes of usage:
+                  |--standalone tasty-file
+                  |-cp whitespace-separated-classpath-entries
+                  |By default, will read the current project.""".stripMargin)
     }
 }
