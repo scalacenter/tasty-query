1454e0fdb8ab5aa9a111d8bab1d585094090ba58
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-10-06 14:58:16 +0200

    Specify an infinite timeout for all MUnit tests.
    Otherwise we cannot debug them.

--- tasty-query/shared/src/test/scala/tastyquery/BaseUnpicklingSuite.scala
+++ tasty-query/shared/src/test/scala/tastyquery/BaseUnpicklingSuite.scala
@@ -8,4 +8,6 @@ import tastyquery.testutil.TestPlatform
 
 abstract class BaseUnpicklingSuite extends munit.FunSuite {
   lazy val testClasspath: Future[Classpath] = TestPlatform.loadClasspath()
+
+  override def munitTimeout = scala.concurrent.duration.Duration.Inf
 }
