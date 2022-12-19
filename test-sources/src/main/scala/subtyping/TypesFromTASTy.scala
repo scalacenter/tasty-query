badf1b6eea0a061428c046ae9191d61c5571d8b0
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-12-19 15:42:01 +0100

    Correctly handle the `scala.|` and `scala.&` aliases in subtyping.
    * They should be defined as `TypeLambda`s, not `PolyType`s.
    * When there is a non-class tycon in an AppliedType on the left
      of a subtyping comparison, we need to try the `superType`, to
      handle the case where it is a `TypeRef` to a polymorphic type
      alias.

--- test-sources/src/main/scala/subtyping/TypesFromTASTy.scala
+++ test-sources/src/main/scala/subtyping/TypesFromTASTy.scala
@@ -4,4 +4,7 @@ class TypesFromTASTy:
   val listDefaultImport: List[Int] = Nil
   val listFullyQualified: scala.collection.immutable.List[Int] = Nil
   val listPackageAlias: scala.List[Int] = Nil
+
+  val orType: Int | String = 1
+  val andType: Product & Serializable = Nil
 end TypesFromTASTy
