scalacenter/tasty-query

commit a8b4fbc7d343a67f416599ace180fa21b7aa6c3c

Implement the relationship between TupleN classes and generic tuples.

tasty-query/shared/src/main/scala/tastyquery/Definitions.scala
@@ -70,6 +70,19 @@ final class Definitions private[tastyquery] (ctx: Context, rootPackage: PackageS
 
   val ThrowableType: TypeRef = TypeRef(javaLangPackage.packageRef, typeName("Throwable"))
 
+  val TupleType: TypeRef = TypeRef(scalaPackage.packageRef, tpnme.Tuple)
+
+  val EmptyTupleType: TermRef =
+    TermRef(TermRef(scalaPackage.packageRef, termName("Tuple$package")), nme.EmptyTuple)
+
+  val NonEmptyTupleType: TypeRef = TypeRef(scalaPackage.packageRef, tpnme.NonEmptyTuple)
+
+  val TupleConsTypeUnapplied: TypeRef = TypeRef(scalaPackage.packageRef, tpnme.TupleCons)
+  def TupleConsTypeOf(head: Type, tail: Type): AppliedType = AppliedType(TupleConsTypeUnapplied, List(head, tail))
+
+  def GenericTupleTypeOf(elementTypes: List[Type]): Type =
+    elementTypes.foldRight[Type](EmptyTupleType)(TupleConsTypeOf(_, _))
+
   // Magic symbols that are not found on the classpath, but rather created by hand
 
   private def createSpecialClass(name: TypeName, parents: List[Type], flags: FlagSet): ClassSymbol =
@@ -378,6 +391,11 @@ final class Definitions private[tastyquery] (ctx: Context, rootPackage: PackageS
     sym == IntClass || sym == LongClass || sym == FloatClass || sym == DoubleClass ||
       sym == BooleanClass || sym == ByteClass || sym == ShortClass || sym == CharClass ||
       sym == UnitClass
+
+  private lazy val TupleNClasses = (1 to 22).map(n => scalaPackage.requiredClass(s"Tuple$n")).toSet
+
+  def isTupleNClass(sym: ClassSymbol): Boolean =
+    sym.owner == scalaPackage && TupleNClasses.contains(sym)
 
   lazy val uninitializedMethod: Option[TermSymbol] =
     scalaCompiletimePackage.getDecl(moduleClassName("package$package")).flatMap { packageObjectClass =>

tasty-query/shared/src/main/scala/tastyquery/Subtyping.scala
@@ -294,8 +294,8 @@ private[tastyquery] object Subtyping:
         else
           tycon2.optSymbol match
             case Some(cls2: ClassSymbol) =>
-              // TODO Handle generic tuple <: TupleN
-              level3WithBaseType(tp1, tp2, cls2)
+              (defn.isTupleNClass(cls2) && isSubtype(tp1, defn.GenericTupleTypeOf(tp2.args)))
+                || level3WithBaseType(tp1, tp2, cls2)
             case Some(sym2: TypeMemberSymbol) if sym2.isTypeAlias =>
               isSubtype(tp1, tp2.superType)
             case _ =>

tasty-query/shared/src/main/scala/tastyquery/reader/pickles/PickleReader.scala
@@ -248,18 +248,21 @@ private[pickles] class PickleReader {
         val tpe = readSymType()
         val typeParams = atNoCache(infoRef)(readTypeParams())
         if isRefinementClass(cls) then return cls // by-pass further assignments, including Flags
+        val scala2ParentTypes = tpe match
+          case TempPolyType(tparams, restpe: TempClassInfoType) =>
+            assert(tparams.corresponds(typeParams)(_ eq _)) // should reuse the class type params
+            restpe.parentTypes
+          case tpe: TempClassInfoType => tpe.parentTypes
+          case tpe =>
+            throw AssertionError(s"unexpected type $tpe for $cls, owner is $owner")
         val parentTypes =
           if cls.owner == defn.scalaPackage && tname == tpnme.AnyVal then
             // Patch the superclasses of AnyVal to contain Matchable
-            defn.AnyType :: defn.MatchableType :: Nil
-          else
-            tpe match
-              case TempPolyType(tparams, restpe: TempClassInfoType) =>
-                assert(tparams.corresponds(typeParams)(_ eq _)) // should reuse the class type params
-                restpe.parentTypes
-              case tpe: TempClassInfoType => tpe.parentTypes
-              case tpe =>
-                throw AssertionError(s"unexpected type $tpe for $cls, owner is $owner")
+            scala2ParentTypes :+ defn.MatchableType
+          else if cls.owner == defn.scalaPackage && isTupleClassName(tname) then
+            // Patch the superclass of TupleN classes to inherit from *:
+            defn.GenericTupleTypeOf(typeParams.map(_.typeRef)) :: scala2ParentTypes.tail
+          else scala2ParentTypes
         val givenSelfType = if atEnd then None else Some(readTypeRef())
         cls.withParentsDirect(parentTypes)
         cls.withTypeParams(typeParams)
@@ -815,5 +818,15 @@ private[reader] object PickleReader {
 
   /** Temporary type for classinfos, will be decomposed on completion of the class */
   private[tastyquery] case class TempClassInfoType(parentTypes: List[Type]) extends CustomTransientGroundType
+
+  private def isTupleClassName(name: TypeName): Boolean =
+    name.toTermName match
+      case SimpleName(str) =>
+        str.startsWith("Tuple")
+          && str.length() > 5
+          && str.iterator.drop(5).forall(c => c >= '0' && c <= '9')
+      case _ =>
+        false
+  end isTupleClassName
 
 }

tasty-query/shared/src/main/scala/tastyquery/Names.scala
@@ -72,6 +72,8 @@ object Names {
     val javaPackageName: SimpleName = termName("java")
     val langPackageName: SimpleName = termName("lang")
 
+    val EmptyTuple: SimpleName = termName("EmptyTuple")
+
     val m_== : SimpleName = termName("==")
     val m_!= : SimpleName = termName("!=")
     val m_## : SimpleName = termName("##")
@@ -118,6 +120,8 @@ object Names {
     val Tuple: TypeName = typeName("Tuple")
     val NonEmptyTuple: TypeName = typeName("NonEmptyTuple")
     val TupleCons: TypeName = typeName("*:")
+
+    @deprecated("you probably meant the term name `nme.EmptyTuple` instead", since = "0.8.3")
     val EmptyTuple: TypeName = typeName("EmptyTuple")
 
     val RefinedClassMagic: TypeName = typeName("<refinement>")

tasty-query/shared/src/test/scala/tastyquery/SubtypingSuite.scala
@@ -1457,4 +1457,31 @@ class SubtypingSuite extends UnrestrictedUnpicklingSuite:
     }
   }
 
+  testWithContext("generic-tuples") {
+    def TupleNType(elementTypes: List[Type]): Type =
+      defn.scalaPackage.packageRef.select(typeName("Tuple" + elementTypes.size)).appliedTo(elementTypes)
+
+    assertStrictSubtype(defn.EmptyTupleType, defn.TupleType)
+    assertNeitherSubtype(defn.EmptyTupleType, defn.NonEmptyTupleType)
+
+    assertStrictSubtype(defn.TupleConsTypeOf(defn.IntType, defn.EmptyTupleType), defn.NonEmptyTupleType)
+    assertStrictSubtype(defn.TupleConsTypeOf(defn.IntType, defn.EmptyTupleType), defn.TupleType)
+    assertNeitherSubtype(defn.TupleConsTypeOf(defn.IntType, defn.EmptyTupleType), defn.EmptyTupleType)
+
+    assertStrictSubtype(TupleNType(List(defn.IntType, defn.StringType)), defn.NonEmptyTupleType)
+
+    assertEquiv(
+      TupleNType(List(defn.IntType, defn.StringType)),
+      defn.GenericTupleTypeOf(List(defn.IntType, defn.StringType))
+    )
+    assertStrictSubtype(
+      TupleNType(List(defn.IntType, defn.StringType)),
+      defn.GenericTupleTypeOf(List(defn.AnyValType, defn.StringType))
+    )
+    assertStrictSubtype(
+      defn.GenericTupleTypeOf(List(defn.IntType, defn.StringType)),
+      TupleNType(List(defn.AnyValType, defn.StringType))
+    )
+  }
+
 end SubtypingSuite

tasty-query/shared/src/test/scala/tastyquery/TypeSuite.scala
@@ -858,6 +858,32 @@ class TypeSuite extends UnrestrictedUnpicklingSuite {
     assert(clue(defn.NothingClass.parentClasses) == List(defn.AnyClass))
   }
 
+  testWithContext("parents-of-tuple-classes") {
+    val SerializableClass = ctx.findTopLevelClass("java.io.Serializable")
+    val MirrorSingletonClass = ctx.findStaticClass("scala.deriving.Mirror.Singleton")
+    val TupleClass = ctx.findTopLevelClass("scala.Tuple")
+    val NonEmptyTupleClass = ctx.findTopLevelClass("scala.NonEmptyTuple")
+    val TConsClass = ctx.findTopLevelClass("scala.*:")
+    val EmptyTupleClass = ctx.findStaticModuleClass("scala.Tuple$package.EmptyTuple")
+    val Tuple2Class = ctx.findTopLevelClass("scala.Tuple2")
+    val ProductClass = ctx.findTopLevelClass("scala.Product")
+    val Product2Class = ctx.findTopLevelClass("scala.Product2")
+
+    assert(clue(TupleClass.parentClasses) == List(defn.ObjectClass, ProductClass))
+    assert(
+      clue(EmptyTupleClass.parentClasses) == List(
+        defn.ObjectClass,
+        TupleClass,
+        ProductClass,
+        SerializableClass,
+        MirrorSingletonClass
+      )
+    )
+    assert(clue(NonEmptyTupleClass.parentClasses) == List(defn.ObjectClass, TupleClass))
+    assert(clue(TConsClass.parentClasses) == List(defn.ObjectClass, NonEmptyTupleClass))
+    assert(clue(Tuple2Class.parentClasses) == List(TConsClass, Product2Class, ProductClass, SerializableClass))
+  }
+
   testWithContext("java-class-signatures-[RecClass]") {
     val RecClass = ctx.findTopLevelClass("javadefined.RecClass")
 

commit 91cac7c4beadf2d6fd2c8e57da793658ce8a8ff7

Restore the ability to work in a Scala 2-only classpath.
Avoid referring to Scala 3 generic tuples if they are not
available on the classpath.

This was broken in a8b4fbc7d343a67f416599ace180fa21b7aa6c3c.

tasty-query/shared/src/main/scala/tastyquery/Definitions.scala
@@ -397,6 +397,8 @@ final class Definitions private[tastyquery] (ctx: Context, rootPackage: PackageS
   def isTupleNClass(sym: ClassSymbol): Boolean =
     sym.owner == scalaPackage && TupleNClasses.contains(sym)
 
+  lazy val hasGenericTuples = scalaPackage.getDecl(tpnme.TupleCons).isDefined
+
   lazy val uninitializedMethod: Option[TermSymbol] =
     scalaCompiletimePackage.getDecl(moduleClassName("package$package")).flatMap { packageObjectClass =>
       packageObjectClass.asClass.getDecl(termName("uninitialized"))

tasty-query/shared/src/main/scala/tastyquery/Subtyping.scala
@@ -294,7 +294,7 @@ private[tastyquery] object Subtyping:
         else
           tycon2.optSymbol match
             case Some(cls2: ClassSymbol) =>
-              (defn.isTupleNClass(cls2) && isSubtype(tp1, defn.GenericTupleTypeOf(tp2.args)))
+              (defn.hasGenericTuples && defn.isTupleNClass(cls2) && isSubtype(tp1, defn.GenericTupleTypeOf(tp2.args)))
                 || level3WithBaseType(tp1, tp2, cls2)
             case Some(sym2: TypeMemberSymbol) if sym2.isTypeAlias =>
               isSubtype(tp1, tp2.superType)

tasty-query/shared/src/main/scala/tastyquery/reader/pickles/PickleReader.scala
@@ -259,7 +259,7 @@ private[pickles] class PickleReader {
           if cls.owner == defn.scalaPackage && tname == tpnme.AnyVal then
             // Patch the superclasses of AnyVal to contain Matchable
             scala2ParentTypes :+ defn.MatchableType
-          else if cls.owner == defn.scalaPackage && isTupleClassName(tname) then
+          else if cls.owner == defn.scalaPackage && isTupleClassName(tname) && defn.hasGenericTuples then
             // Patch the superclass of TupleN classes to inherit from *:
             defn.GenericTupleTypeOf(typeParams.map(_.typeRef)) :: scala2ParentTypes.tail
           else scala2ParentTypes