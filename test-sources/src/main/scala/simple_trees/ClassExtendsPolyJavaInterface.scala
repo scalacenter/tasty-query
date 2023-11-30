56c7da05a0a1dab11ca055e42beea2872696821e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-11-30 16:20:19 +0100

    Fix #407: Synthesize a constructor for Java interfaces.

---
+++ test-sources/src/main/scala/simple_trees/ClassExtendsPolyJavaInterface.scala
@@ -0,0 +1,5 @@
+package simple_trees
+
+class ClassExtendsPolyJavaInterface extends java.util.function.Predicate[String]():
+  def test(t: String): Boolean = t.isEmpty()
+end ClassExtendsPolyJavaInterface
